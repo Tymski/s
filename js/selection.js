c0b67c2850a8803d140d954a7e77c2bf67ba9472
-m <Tymon.Oziemblewski@gmail.com>
2019-01-29 01:37:51 +0100

    8558

--- js/selection.js
+++ js/selection.js
@@ -1,4 +1,5 @@
 body = document.querySelector("body");
+icons = document.querySelectorAll(".icon-link");
 
 selectedArea = {};
 
@@ -8,6 +9,7 @@ body.addEventListener("poinermove", drawRectangleMove);
 body.addEventListener("mousemove", drawRectangleMove);
 body.addEventListener("pointerup", drawRectangleEnd);
 body.addEventListener("mouseup", drawRectangleEnd);
+body.addEventListener("onblur", drawRectangleCancel);
 
 function drawRectangleStart(event) {
     selectedArea.x = event.x;
@@ -25,6 +27,10 @@ function drawRectangleEnd(event) {
     selectedArea.active = false;
 }
 
+function drawRectangleCancel(event) {
+    selectedArea.active = false;
+}
+
 function setSelectedAreaDiv() {
     selectedAreaDiv.style.width = Math.abs(selectedArea.width) + "px";
     selectedAreaDiv.style.height = Math.abs(selectedArea.height) + "px";
@@ -35,4 +41,21 @@ function setSelectedAreaDiv() {
     } else {
         selectedAreaDiv.classList.add("hidden");
     }
+    collideAll();
+}
+
+function rectCollision(rect1, rect2) {
+    if (rect1.left > rect2.right || rect2.left > rect1.right) return false;
+    if (rect1.top > rect2.bottom || rect2.top > rect1.bottom) return false;
+    return true;
+}
+
+function collideAll() {
+    icons.forEach(icon => {
+        if (rectCollision(icon.getBoundingClientRect(), selectedAreaDiv.getBoundingClientRect())) {
+            icon.classList.add("selected");
+        }else{
+            icon.classList.remove("selected");
+        }
+    });
 }
